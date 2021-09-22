e1eee29e7c6f4c2893a85ddcedfab778b2c07efb
wutlu <mutlu@etsetra.com>
2021-09-22 23:01:48 +0300

    Added lorem class

--- src/DateTime.php
+++ src/DateTime.php
@@ -1,12 +1,9 @@
 <?php
 
 /*
- * This file is part of etsetra/DT
+ * This file is part of etsetra/Library
  *
  * (c) Etsetra <hello@etsetra.com>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
  */
 namespace etsetra\Library;
 
